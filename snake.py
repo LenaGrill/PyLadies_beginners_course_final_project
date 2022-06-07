46c1706a30881213867628dc04ac6cc18645638a
Lena <mgdgri@gmail.com>
2022-06-07 10:05:31 +0200

    some cleanup done

--- snake.py
+++ snake.py
@@ -1,21 +1,7 @@
 from random import randrange
 
-# def initial_map(x_axis, y_axis):
-    # """creates a list of lists with x_axis items and y-axis rows;
-    #    the method seems to be correct and the resulting list is fine, 
-    #    but cannot be used, as the replacement of dot by X doesn't work properly"""
-    # row = []
-    # clear_map = []
-    # for positions in range(x_axis):
-    #     row.append(".")
-    # # print(row)
-    # for lines in range(y_axis):
-    #     clear_map.append(row)
-    # return clear_map
-
 def initial_map(n_rows, n_columns):
-     """creates a list of lists with n_columns items and n_rows rows
-     """
+     """creates a list of lists with n_columns items and n_rows rows """
      clear_map = []
      for _ in range(n_rows):
          clear_map.append(["."] * n_columns)
@@ -28,15 +14,12 @@ def new_map(input_map, coordinates):
     # print(a)
     # print(coordinates)
     for x, y in coordinates:
-
         # print(x, y)       
         draw_map[x][y] = "X"
-       
     return draw_map, coordinates
 
 
 def movement(coordinates, direction, draw_map, n_rows, n_columns):
-
     i = coordinates[-1][0]
     j = coordinates[-1][1]
     # print(i, j)
@@ -80,12 +63,10 @@ def movement(coordinates, direction, draw_map, n_rows, n_columns):
             return coordinates
 
 def snakefood(draw_map, n_rows, n_columns):
-    
     while True:
         snakefood_row = randrange(0, n_rows)
         snakefood_column = randrange(0, n_columns)
         print(snakefood_row, snakefood_column)
-
         if draw_map[snakefood_row][snakefood_column] == ".":
             draw_map[snakefood_row][snakefood_column] = "O"
             return draw_map
@@ -93,10 +74,8 @@ def snakefood(draw_map, n_rows, n_columns):
             True
 
 def map(n_rows, n_columns):
-    
     a = initial_map(n_rows, n_columns) # empty map is called
     # print(a)
-    
     # List the table clear_map:
     # for row in a:
     #     for number in row:
@@ -105,7 +84,6 @@ def map(n_rows, n_columns):
     coordinates = [[0,0],[0,1],[0,2]] # these are the starting coordinates for the snake
     draw_map, coordinates = new_map(a, coordinates)
     draw_map = snakefood(draw_map, n_rows, n_columns) # fist snakefood should be already in the map, therefore new_map and snakefood have to be called before while loop
-
     while True:
         # print(a)
         draw_map, coordinates = new_map(a, coordinates) 
@@ -115,9 +93,7 @@ def map(n_rows, n_columns):
             for number in row:
                 print(number, end = "")
             print()
-
         direction = input("Please select a movement direction: north = n, east = e, south = s, west = w. End the game = end. ")
-    
         c = movement(coordinates, direction, draw_map, n_rows, n_columns)
         # print(a)
         if not c:
